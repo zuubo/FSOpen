b7e3d2d72123150b2c319ef773d419ddce777b9d
William Zhang <zhangy62@mcmaster.ca>
2019-06-12 10:36:18 +0800

    finished reading 1a, starting the exercises

---
+++ part1/part1/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Dude" age={26 + 10}/>
+      <Hello name={name} age={age}/>
+
+
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
