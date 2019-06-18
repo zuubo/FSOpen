7491a66d6d42ad4875f2ea5c286b3fc24dad6a90
William Zhang <zhangy62@mcmaster.ca>
2019-06-18 16:04:40 +0800

    read through part 1c -not exactly sure what I learned

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,12 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  console.log('from Header: ' + props.course)
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part1 p1={props.parts[0].name} e1={props.parts[0].exercises}/>
+      <Part2 p2={props.parts[1].name} e2={props.parts[1].exercises}/>
+      <Part3 p3={props.parts[2].name} e3={props.parts[2].exercises}/>
+    </>
+  )
+}
+
+const Part1 = (props) => {
+  return (
+    <>
+      <p>
+        {props.p1} {props.e1}
+      </p>
+    </>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <>
+      <p>
+        {props.p2} {props.e2}
+      </p>
+    </>
+  )
+}
+
+const Part3 = (props) => {
+  return (
+    <>
+      <p>
+        {props.p3} {props.e3}
+      </p>
+    </>
+  )
+}
+
+
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </>
+  )
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -1,30 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
+  const setToValue = (value) => setCounter(value)
+
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <Display counter={counter}/>
+      <Button
+        onClick={() => setToValue(counter + 1)}
+        text='plus'
+      />
+      <Button
+        onClick={() => setToValue(counter - 1)}
+        text='minus'
+      />
+      <Button
+        onClick={() => setToValue(0)}
+        text='zero'
+      />
     </div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
+//   setTimeout(
+//     () => setCounter(counter + 1),
+//     1000
+//   )
+//
+// console.log('rendering...', counter)
+//
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
 
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Dude" age={26 + 10}/>
-      <Hello name={name} age={age}/>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
 
 
-    </>
-  )
-}
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age
+//
+//   return (
+//     <div>
+//       <p>
+//         Hello {props.name}, you are {props.age} years old
+//       </p>
+//       <p>So you were probably born {bornYear()}</p>
+//     </div>
+//   )
+// }
+//
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+//
+//   return (
+//     <>
+//       <h1>Greetings</h1>
+//       <Hello name="Dude" age={26 + 10}/>
+//       <Hello name={name} age={age}/>
+//
+//
+//     </>
+//   )
+// }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
